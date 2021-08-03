aca9edfa936ab5dfe9fec2385d95663cf7db8e87
Setuna-y <kykyfmfm0725@gmail.com>
2021-08-03 16:37:34 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, days_until_shipping_id, :price).merge(user_id: current_user.id)
+  end
+
 end

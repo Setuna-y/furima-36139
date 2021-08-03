63c7f6dcb058f3d8a7281169e2fea3c28f0b7677
Setuna-y <kykyfmfm0725@gmail.com>
2021-08-03 17:50:57 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :days_until_shipping_id
+    validates :price, numericality: { in:  300..9999999 }
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :days_until_shipping
+
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 end

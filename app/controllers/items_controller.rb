d821b70dcab535c81d04479b1ec0cc20b9591d4b
Setuna-y <kykyfmfm0725@gmail.com>
2021-08-10 16:36:01 +0900

    commit 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
   
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -19,10 +20,30 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    item.update
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    item.destroy
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
                                  :days_until_shipping_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end

803117763fd4861f00bdeeefde46e65da8cf011a
Setuna-y <kykyfmfm0725@gmail.com>
2021-08-10 19:17:44 +0900

    commit1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: %i[index show]
-  before_action :set_item, only: %i[show]
+  before_action :authenticate_user!, except: %i[index show edit]
 
   def index
     @items = Item.all.order(created_at: 'DESC')
@@ -20,28 +19,29 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    @item = Item.find(params[:id])
+  end
 
-  #def edit; end
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path if @item.user != current_user
+  end
 
-  #def update
-    #@item = Item.find(params[:id])
-    #item.update
-  #end
+  def update
+    @item = Item.find(params[:id])
+    item.update(item_params)
+  end
 
-  #def destroy
-    #@item = Item.find(params[:id])
-    #item.destroy
-  #end
+  # def destroy
+  # @item = Item.find(params[:id])
+  # item.destroy
+  # end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
                                  :days_until_shipping_id, :price).merge(user_id: current_user.id)
-  end
-
-  def set_item
-    @item = Item.find(params[:id])
   end
 end
